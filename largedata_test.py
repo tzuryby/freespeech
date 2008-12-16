46ac64e5434cebf39a345dce0245b47a2f522c21
tzury bar yochay <tzury@regulus.(none)>
2008-12-16 06:31:21 +0200

    added large data (4kb) sample for twisted

---
+++ largedata_test.py
@@ -0,0 +1,38 @@
+from twisted.internet.protocol import DatagramProtocol
+from twisted.internet import reactor
+
+# Here's a UDP version of the simplest possible protocol
+class UDPServer(DatagramProtocol):
+    def datagramReceived(self, datagram, address):
+        print 'received: %d', len(datagram)
+
+
+class UDPClient(DatagramProtocol):
+    
+    def startProtocol(self):
+        self.transport.connect('127.0.0.1', 8000)
+        self.sendDatagram()
+    
+    def sendDatagram(self):
+        lines = (line for line in open('large_data.rtp', 'r'))
+        for line in lines:
+            self.transport.write(line)
+        
+        #reactor.stop()
+
+    def datagramReceived(self, datagram, host):
+        print 'Datagram received: ', repr(datagram)
+        self.sendDatagram()
+
+
+def main():
+    reactor.listenUDP(8000, UDPServer())
+    protocol = UDPClient()
+    t = reactor.listenUDP(0, protocol)    
+    reactor.run()
+
+
+
+
+if __name__ == '__main__':
+    main()
